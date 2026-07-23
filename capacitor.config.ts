41c1f77b032d300e1410af71cc5edbc6e131d920
Arturo Santana <santana@MacBook-Pro-de-Arturo.local>
2026-07-23 19:48:20 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'colmado-angular-frontend',
+  webDir: 'www'
+};
+
+export default config;
